fcff7c7559e692cf1100bac4dca50580e1377dc8
UM1985 <56utkarshpatel@gmail.com>
2025-04-12 18:24:48 +0530

    game almost done

--- code.cpp
+++ code.cpp
@@ -34,71 +34,56 @@ void Game::display_board()
 void Game::player_turn()
 {
     int choice;
+    bool validMove = false;
 
-    if (turn == 'X')
+    while (!validMove)  // keep asking until a valid move is made
     {
-        cout << "\n\t Player1 [X] turn : ";
-    }
-    if (turn == 'O')
-    {
-        cout << "\n\tPlayer2 [O] trun : ";
-    }
+        if (turn == 'X')
+        {
+            cout << "\n\t Player1 [X] turn : ";
+        }
+        else
+        {
+            cout << "\n\t Player2 [O] turn : ";
+        }
 
-    cin >> choice;
+        cin >> choice;
 
-    switch (choice)
-    {
-    case 1:
-        row = 0;
-        column = 0;
-        break;
-    case 2:
-        row = 0;
-        column = 1;
-        break;
-    case 3:
-        row = 0;
-        column = 2;
-        break;
-    case 4:
-        row = 1;
-        column = 0;
-        break;
-    case 5:
-        row = 1;
-        column = 1;
-        break;
-    case 6:
-        row = 1;
-        column = 2;
-        column = 0;
-        break;
-    case 7:
-        row = 2;
-        break;
-    case 8:
-        row = 2;
-        column = 1;
-        break;
-    case 9:
-        row = 2;
-        column = 2;
-        break;
+        switch (choice)
+        {
+        case 1:
+            row = 0; column = 0; break;
+        case 2:
+            row = 0; column = 1; break;
+        case 3:
+            row = 0; column = 2; break;
+        case 4:
+            row = 1; column = 0; break;
+        case 5:
+            row = 1; column = 1; break;
+        case 6:
+            row = 1; column = 2; break;
+        case 7:
+            row = 2; column = 0; break;
+        case 8:
+            row = 2; column = 1; break;
+        case 9:
+            row = 2; column = 2; break;
+        default:
+            cout << "Invalid Choice. Try again.\n";
+            continue;
+        }
 
-    default:
-        cout << "Invalid Choice \n";
-        break;
-    }
-
-    if (turn == 'X')
-    {
-        Board[row][column] = 'X';
-        turn = 'O';
-    }
-    else if (turn == 'O')
-    {
-        Board[row][column] = 'O';
-        turn = 'X';
+        if (Board[row][column] != 'X' && Board[row][column] != 'O')
+        {
+            Board[row][column] = turn;
+            turn = (turn == 'X') ? 'O' : 'X';
+            validMove = true; 
+        }
+        else
+        {
+            cout << "Box already filled! Choose another one.\n";
+        }
     }
 }
 bool Game::game_over()
@@ -156,3 +141,4 @@ int main()
 
     return 0;
 }
+
